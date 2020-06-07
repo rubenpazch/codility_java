a52293bb3744b2e6a855af3c7ea88feffb50196f
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-06-07 12:48:27 -0500

    add initial file for java project

---
+++ src/codility_java/test_cases.java
@@ -0,0 +1,10 @@
+package codility_java;
+
+public class test_cases {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test main file");
+	}
+
+}
